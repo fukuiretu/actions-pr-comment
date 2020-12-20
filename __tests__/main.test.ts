1c00199aa6ab05efb856769190d9526eb0404348
fukuiretu <fukuiretu@gmail.com>
2020-12-20 23:38:59 +0900

    test

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,28 +1,39 @@
-import {wait} from '../src/wait'
+import {run} from '../src/run'
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import nock from 'nock'
 import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
-})
+beforeEach(() => {
+  jest.resetModules()
 
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
-})
-
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
-  const np = process.execPath
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecFileSyncOptions = {
-    env: process.env
+  github.context.payload = {
+    action: 'opened',
+    pull_request: {
+      number: 1
+    }
   }
-  console.log(cp.execFileSync(np, [ip], options).toString())
+})
+
+test('comments on PR', async () => {
+  process.env['INPUT_REPO-TOKEN'] = 'test-github-token'
+  process.env['INPUT_MESSAGE'] = 'Test Comment'
+  process.env['GITHUB_REPOSITORY'] = 'testowner/testrepo'
+
+  nock('https://api.github.com')
+    .post(
+      '/repos/testowner/testrepo/issues/1/comments',
+      body => body.body === 'Test Comment'
+    )
+    .reply(200, {
+      html_url: 'https://github.com/testowner/testrepo/issues/1#issuecomment-1'
+    })
+  const setOutputMock = jest.spyOn(core, 'setOutput')
+
+  await run()
+
+  expect(setOutputMock).toHaveBeenCalledWith(
+    'comment-url',
+    'https://github.com/testowner/testrepo/issues/1#issuecomment-1'
+  )
 })
